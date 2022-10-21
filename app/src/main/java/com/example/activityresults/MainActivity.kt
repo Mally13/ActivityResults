c82b948aacb9f6539461a16c1127a121c5d26c22
Mally13 <mallymutuku13@gmail.com>
2022-10-21 15:26:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/activityresults/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.activityresults
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+
+//const val PICK_RAINBOW_COLOR_INTENT = 1  // The request code
+const val RAINBOW_COLOR_NAME = "RAINBOW_COLOR_NAME" // Key to return rainbow color name in intent
+const val RAINBOW_COLOR = "RAINBOW_COLOR" // Key to return rainbow color in intent
+const val DEFAULT_COLOR = "#FFFFFF" // White
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.submit_button).setOnClickListener {
+            //Set the name of the Activity to launch passing
+            //in request code
+
+            val intent= Intent(this, RainbowColorPickerActivity::class.java)
+            getResult.launch(intent)
+
+        }
+
+    }
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val backgroundColor = it.data?.getIntExtra(RAINBOW_COLOR,
+                    Color.parseColor(DEFAULT_COLOR)) ?:
+                Color.parseColor(DEFAULT_COLOR)
+                val colorName = it.data?.getStringExtra(RAINBOW_COLOR_NAME) ?: ""
+                val colorMessage = getString(R.string.color_chosen_message, colorName)
+                val rainbowColor = findViewById<TextView>(R.id.rainbow_color)
+                rainbowColor.setBackgroundColor(ContextCompat.getColor(this, backgroundColor))
+                rainbowColor.text = colorMessage
+                rainbowColor.isVisible = true
+
+
+            }
+        }
+
+}
